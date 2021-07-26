f6c185ea2178dfa745759722242e01af8aa45086
okatoku331 <okatoku331@gmail.com>
2021-07-26 11:21:15 +0900

    第19版　スターターキット終了

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List<String> titleList = ['google', '楽天', 'amazon'];
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
